51bcb680b8202a30912f26825bd7ea58509f0c53
Sotha <minesotha@gmail.com>
2018-04-13 10:11:58 +0200

    lot of changes
    getting data from time window is not working, wrong idea :(

---
+++ JINS/BoardWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace JINS
+{
+    /// <summary>
+    /// Interaction logic for BoardWindow.xaml
+    /// </summary>
+    public partial class BoardWindow : Window
+    {
+        private string currentRadioCheckedName = "11";
+        public BoardWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void centralize_click(object sender, RoutedEventArgs e)
+        {
+            radio_11.IsChecked = true;
+            currentRadioCheckedName = "11";
+        }
+
+        public void SetRadio(string direction)
+        {
+            int row = int.Parse(currentRadioCheckedName[0].ToString());
+            int column = int.Parse(currentRadioCheckedName[1].ToString());
+            if (direction == "left" && column>0)
+            {
+                column--;
+            }
+            if(direction== "right" && column < 2)
+            {
+                column++;
+            }
+            if (direction == "up" && row > 0)
+            {
+                row--;
+            }
+            if (direction == "down" && row < 2)
+            {
+                row++;
+            }
+            currentRadioCheckedName = string.Concat(row, column);
+
+            switch (currentRadioCheckedName)
+            {
+                case "00":
+                    {
+                        radio_00.IsChecked = true;
+                        break;
+                    }
+                case "01":
+                    {
+                        radio_01.IsChecked = true;
+                        break;
+                    }
+                case "02":
+                    {
+                        radio_02.IsChecked = true;
+                        break;
+                    }
+
+                case "10":
+                    {
+                        radio_10.IsChecked = true;
+                        break;
+                    }
+                case "11":
+                    {
+                        radio_11.IsChecked = true;
+                        break;
+                    }
+                case "12":
+                    {
+                        radio_12.IsChecked = true;
+                        break;
+                    }
+
+                case "20":
+                    {
+                        radio_20.IsChecked = true;
+                        break;
+                    }
+                case "21":
+                    {
+                        radio_21.IsChecked = true;
+                        break;
+                    }
+                case "22":
+                    {
+                        radio_22.IsChecked = true;
+                        break;
+                    }
+
+            }
+        }
+    }
+}
--- JINS/MainWindow.xaml.cs
+++ JINS/MainWindow.xaml.cs
@@ -1,37 +1,94 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace JINS
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            //senderSock.Close();
-        }
+        #region CONSTS
+        //strings
+        const string NEUTRAL = "neutral";
+        const string BIG_UP = "big up";
+        const string BIG_DOWN = "big down";
+        const string BIG_LEFT = "big left";
+        const string BIG_RIGHT = "big right";
+        const string SMALL_UP = "small up";
+        const string SMALL_DOWN = "small down";
+        const string SMALL_LEFT = "small left";
+        const string SMALL_RIGHT = "small right";
+        const string NOISE = "noise";
 
+        //options
+        const int TIME_WINDOW = 200;
+        const float MIN_POSITIVES_TO_DECLARE_MOVE = 0.8f; 
+        #endregion
+
+        //okno do testowania gdzie sa galki oczne
+        BoardWindow bWin;
+
+        public SeriesCollection Series { get; set; }
         static byte[] bytes = new byte[1024];
         static Socket senderSock;
         bool stopCondition = false;
+        Queue<int> vQueue;
+        Queue<int> hQueue;
+        string lastHString;
+        string lastVstring;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+            vQueue = new Queue<int>(TIME_WINDOW);
+            hQueue = new Queue<int>(TIME_WINDOW);
+
+            Series = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    //AreaLimit = -10,
+                    Values = new ChartValues<ObservableValue>
+                    {
+                        new ObservableValue(100),
+                        new ObservableValue(200),
+                        new ObservableValue(300),
+                        new ObservableValue(100),
+                        new ObservableValue(300),
+                        new ObservableValue(4),
+                        new ObservableValue(2),
+                        new ObservableValue(5),
+                        new ObservableValue(8),
+                        new ObservableValue(3),
+                        new ObservableValue(5),
+                        new ObservableValue(6),
+                        new ObservableValue(7),
+                        new ObservableValue(3),
+                        new ObservableValue(4),
+                        new ObservableValue(2),
+                        new ObservableValue(5),
+                        new ObservableValue(8)
+                    }
+                }
+            };
+            //senderSock.Close();
+        }
+
 
         public void DoSocketGet()
         {
@@ -79,7 +136,7 @@ namespace JINS
                 while (!stopCondition)
                 {
                     bytesRec = senderSock.Receive(bytes);
-                    ShowData(Encoding.UTF8.GetString(bytes, 0, bytesRec));
+                    PrepareData(Encoding.UTF8.GetString(bytes, 0, bytesRec));
                 }
 
             }
@@ -94,6 +151,25 @@ namespace JINS
             }
         }
 
+        string currentLine;
+        private void PrepareData(string raw)
+        {
+            char c;
+            for (int i = 0; i < raw.Length; i++)
+            {
+                c = raw[i];
+                if(c == '\n')
+                {
+                    ShowData(currentLine);
+                    currentLine = "";
+                }
+                else
+                {
+                    currentLine += raw[i];
+                }
+            }
+        }
+
         private void ShowData(string raw)
         {
             this.Dispatcher.Invoke(() =>
@@ -101,13 +177,335 @@ namespace JINS
                 string[] data = raw.Split(',');
                 if (axisH_radio.IsChecked==true)
                 {
-                    rawText.Text = data[5];
+                    rawText.Text = data[10]+","+data[11];
+
                 }
                 else if(axisV_radio.IsChecked==true){
-                     rawText.Text = data[5];
+                     rawText.Text = data[12]+","+data[13];
                 }
+                PopulateVtext(data[12], data[13]);
+                PopulateHtext(data[10], data[11]);
             });
         }
+
+        private void PlotData(double data)
+        {
+            Action action = delegate
+            {
+                Series[0].Values.Add(new ObservableValue(data));
+                OnPropertyChanged("Series");
+                if (Series[0].Values.Count >= TIME_WINDOW)
+                {
+                    Series[0].Values.RemoveAt(0);
+                }
+            };
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Normal, action);
+        }
+
+        static double alpha = 0.5;
+        static int EMA_window=100;
+        double lastFiltered_V = 0.0;
+        double index_V = 0;
+        double lastFiltered_H = 0.0;
+        double index_H = 0;
+        double[] sma_h = new double[10];
+        double[] sma_v = new double[10];
+        int sma_index_h = 0;
+        int sma_index_v = 0;
+        private double applySMA(double val, bool isV)
+        {
+            if (isV)
+            {
+                if (index_V < sma_v.Length)
+                {
+                    lastFiltered_V = val;
+                    sma_v[sma_index_v] = lastFiltered_V;
+                    sma_index_v++;
+                    index_V++;
+                    return val;
+                }
+                else
+                {
+                    if (sma_index_v == sma_v.Length)
+                    {
+                        sma_index_v = 0;
+                    }
+                    sma_v[sma_index_v] = val;
+                    lastFiltered_V = sma_v.Average();
+                    sma_index_v++;
+                    return lastFiltered_V;
+                }
+            }
+            else
+            {
+                if (index_H < sma_h.Length)
+                {
+                    lastFiltered_H = val;
+                    sma_h[sma_index_h] = lastFiltered_H;
+                    sma_index_h++;
+                    index_H++;
+                    return val;
+                }
+                else
+                {
+                    if (sma_index_h == sma_h.Length)
+                    {
+                        sma_index_h = 0;
+                    }
+                    sma_h[sma_index_h] = val;
+                    lastFiltered_H = sma_h.Average();
+                    sma_index_h++;
+                    return lastFiltered_H;
+                }
+            }
+        }
+
+        private double applyEMA(double val, bool isV)
+        {
+            if (isV)
+            {
+                if (index_V < 1)
+                {
+                    lastFiltered_V = val;
+                    index_V++;
+                    return val;
+                }
+                 else if (index_V < EMA_window)
+                {
+                    lastFiltered_V = (lastFiltered_V+val)/2;
+                    index_V++;
+                    return val;
+                }
+                else
+                {
+                    lastFiltered_V = ((alpha * lastFiltered_V) + ((1-alpha)*val));
+                    return lastFiltered_V;
+                }
+            }
+            else
+            {
+                if (index_H < 1)
+                {
+                    lastFiltered_H = val;
+                    index_H++;
+                    return val;
+                }
+                else if (index_H < EMA_window)
+                {
+                    lastFiltered_H = (lastFiltered_H + val) / 2;
+                    index_H++;
+                    return val;
+                }
+                else
+                {
+                    lastFiltered_H = ((alpha * lastFiltered_H) + ((1 - alpha) * val));
+                    return lastFiltered_H;
+                }
+
+            }
+
+        }      
+
+        private string DetermineV(string right, string left)
+        {
+            int v1;
+            int v2;
+            if (int.TryParse(right, out v1) && int.TryParse(left, out v2))
+            {
+                //średnia z obu oczu
+                int avg = (v1 + v2) / 2;
+                //nałóż filtr
+                if (sma_radio.IsChecked==true)
+                {
+                    avg = (int)applySMA(avg, true);
+                }
+                else
+                {
+                    avg = (int)applyEMA(avg, true);
+                }
+                vQueue.Enqueue(avg);
+
+                if (axisV_radio.IsChecked==true)
+                    PlotData(avg);
+                if (vQueue.Count >= TIME_WINDOW)
+                {
+                    vQueue.Dequeue();
+                }
+
+                return GuessV(avg);
+            }
+            return "?";
+        }
+
+        private string GuessV(int value)
+        {
+
+            if (CheckIfInRange(-80,-10,true))
+            {
+                return NEUTRAL;
+            }
+            else if (CheckIfInRange(-120,-80,true))
+            {
+                MoveRadios(BIG_DOWN);
+                return BIG_DOWN;
+            }
+            else if (CheckIfInRange(30,100,true))
+            {
+                MoveRadios(BIG_UP);
+                return BIG_UP;
+            }
+            else if (CheckIfInRange(-120,-80,true))
+            {
+                MoveRadios(SMALL_DOWN);
+                return SMALL_DOWN;
+            }
+            else if (CheckIfInRange(-30,-10,true))
+            {
+                MoveRadios(SMALL_UP);
+                return SMALL_UP;
+            }
+            else
+            {
+                return NOISE;
+            }
+        }
+
+        private string DetermineH(string right, string left)
+        {
+            int v1;
+            int v2;
+            if (int.TryParse(right, out v1) && int.TryParse(left, out v2))
+            {
+                //średnia z obu oczu
+                int avg = (v1 + v2) / 2;
+                //nałóż filtr
+                if (sma_radio.IsChecked==true)
+                {
+                    avg = (int)applySMA(avg, false);
+                }
+                else
+                {
+                    avg = (int)applyEMA(avg, false);
+                }
+                hQueue.Enqueue(avg);
+                if (axisH_radio.IsChecked == true)
+                    PlotData(avg);
+
+                if (hQueue.Count >=TIME_WINDOW)
+                {
+                    hQueue.Dequeue();
+                }
+
+                return GuessH(avg);
+            }
+            return "?";
+        }
+
+        private string GuessH(int value)
+        {
+
+            if (CheckIfInRange(280, 420, false))
+            {
+                return NEUTRAL;
+            }
+            else if (CheckIfInRange(260, 280, false))
+            {
+                MoveRadios(SMALL_LEFT);
+                return SMALL_LEFT;
+            }
+            else if (CheckIfInRange(420,450,false))
+            {
+                MoveRadios(SMALL_RIGHT);
+                return SMALL_RIGHT;
+            }
+            else if (CheckIfInRange(220,260,false))
+            {
+                MoveRadios(BIG_LEFT);
+                return BIG_LEFT;
+            }
+            else if (CheckIfInRange(450,520,false))
+            {
+                MoveRadios(BIG_RIGHT);
+                return BIG_RIGHT;
+            }
+            else
+            {
+                return NOISE;
+            }
+        }
+        /// <summary>
+        /// sprawdza, czy procent danych w oknie czasowym jest w zakresie podanego ruchu
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <param name="isV"></param>
+        /// <returns></returns>
+        bool CheckIfInRange(int min , int max, bool isV=false)
+        {
+            int positives = 0;
+            float percentage = 0f;
+            if (isV)
+            {
+                foreach(int i in vQueue)
+                {
+                    if(i>min && i < max)
+                    {
+                        positives++;
+                    }
+                }
+                percentage = (float)positives / (float)vQueue.Count;
+                if (percentage > MIN_POSITIVES_TO_DECLARE_MOVE)
+                {
+                    return true;
+                }
+                else return false;
+            }
+            else
+            {
+                foreach (int i in hQueue)
+                {
+                    if (i > min && i < max)
+                    {
+                        positives++;
+                    }
+                }
+                percentage = (float)positives / (float)hQueue.Count;
+                if (percentage > MIN_POSITIVES_TO_DECLARE_MOVE)
+                {
+                    return true;
+                }
+                else return false;
+            }
+        }
+
+        private void PopulateVtext(string right, string left)
+        {
+            string text = DetermineV(left, right);
+            if (text != "?")
+            {
+                V_label.Content = text;
+            }
+        }
+
+        private void PopulateHtext(string right, string left)
+        {
+            string text = DetermineH(left, right);
+            if (text != "?")
+            {
+                H_label.Content = text;
+            }
+        }
+
+        private void MoveRadios(string direction)
+        {
+            if(bWin !=null && bWin.IsVisible)
+            {
+                bWin.SetRadio(direction);
+            }
+        }
+
+
+        #region Events
 
         private void startBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -119,5 +517,29 @@ namespace JINS
         {
             stopCondition = true;
         }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void axis_radio_Checked(object sender, RoutedEventArgs e)
+        {
+            if(Series!=null && Series[0]!=null && Series[0].Values!=null)
+            Series[0].Values.Clear();
+        }
+
+        private void showBoard_Click(object sender, RoutedEventArgs e)
+        {
+
+            //initializing test window
+            bWin = new BoardWindow();
+            bWin.Show();
+            bWin.Owner = this;
+        }
+        #endregion
     }
 }
